7b600cf2e3938a4e5d8f2f6623c545d5828e29e2
Dipanjan <Dipanjan>
2021-05-25 02:06:21 +0530

    Updated Tagline and removed maps

--- components/homepage/About.vue
+++ components/homepage/About.vue
@@ -5,97 +5,106 @@
 			<a-row type="flex">
 				<a-col :xs="24" :md="16" class="about__grid--text">
 					{{ this.$store.getters.getAboutMe }}
-					<p class="about__grid--text__location"><span class="about__grid--text__location--title">Current Location:</span> {{ this.$store.getters.getCurrentLocation }}</p>
+					<p class="about__grid--text__location">
+						<span class="about__grid--text__location--title"
+							>Current Location:</span
+						>
+						{{ this.$store.getters.getCurrentLocation }}
+					</p>
 				</a-col>
 				<a-col :xs="24" :md="8" class="about__grid--image">
-					<img class="profile-picture" src="../../assets/dp.png" alt="Profile Picture">
+					<img
+						class="profile-picture"
+						src="../../assets/dp.png"
+						alt="Profile Picture"
+					/>
 				</a-col>
 			</a-row>
-		</div>
-		<div class="about__map">
-			<iframe class="about__map--frame" frameborder="0" :src="mapSource"></iframe>
 		</div>
 	</div>
 </template>
 
 <script>
-	import { mapsKey } from '@/static/mapsKey.secret.js';
+import { mapsKey } from "@/static/mapsKey.secret.js";
 
-	export default {
-		name: 'About',
-		data() {
-			return {
-				mapSource: ''
-			}
-		},
-		mounted() {
-			this.mapSource = 'https://www.google.com/maps/embed/v1/place?q=place_id:ChIJbU60yXAWrjsR4E9-UejD3_g&key=' + mapsKey;
-		}
-	}
+export default {
+	name: "About",
+	data() {
+		return {
+			mapSource: "",
+		};
+	},
+	mounted() {
+		this.mapSource =
+			"https://www.google.com/maps/embed/v1/place?q=place_id:ChIJbU60yXAWrjsR4E9-UejD3_g&key=" +
+			mapsKey;
+	},
+};
 </script>
 
 <style lang="scss" scoped>
-	.about {
-		background: #f5f5f5;
-		widows: 100vw;
-		margin-top: -25vh;
-		margin-bottom: 0;
-		padding: 9rem 0 0 0;
+.about {
+	background: #f5f5f5;
+	widows: 100vw;
+	margin-top: -25vh;
+	margin-bottom: 0;
+	padding: 9rem 0 0 0;
 
-		&__header {
-			font-size: 1.5rem;
-			text-transform: uppercase;
-			font-weight: 900;
-			letter-spacing: .3rem;
-			background-image: linear-gradient(to right, #C33764, #1D2671);
-			-webkit-background-clip: text;
-			color: transparent;
-			padding: 2rem;
-			cursor: default;
-			text-shadow: .5rem 1rem 2rem rgba(#000, .6);
-		}
+	&__header {
+		font-size: 1.5rem;
+		text-transform: uppercase;
+		font-weight: 900;
+		letter-spacing: 0.3rem;
+		background-image: linear-gradient(to right, #c33764, #1d2671);
+		-webkit-background-clip: text;
+		color: transparent;
+		padding: 2rem;
+		cursor: default;
+		text-shadow: 0.5rem 1rem 2rem rgba(#000, 0.6);
+	}
 
-		&__grid {
-			padding: 2rem;
+	&__grid {
+		padding: 2rem;
 
-			&--text {
-				font-size: 1.2rem;
-				text-align: justify;
-				padding-right: 1.5rem;
-				line-height: 2.5rem;
+		&--text {
+			font-size: 1.2rem;
+			text-align: justify;
+			padding-right: 1.5rem;
+			line-height: 2.5rem;
 
-				&__location {
-					margin-top: 1.2rem;
+			&__location {
+				margin-top: 1.2rem;
 
-					&--title {
-						font-weight: 800;
-					}
+				&--title {
+					font-weight: 800;
 				}
 			}
-
-			&--image {
-				text-align: center;
-			}
 		}
 
-		&__map {
-			display: block;
-			padding: 0;
-			margin: 0;
-
-			&--frame {
-				width: 100%;
-				height: 40vh;
-			}
+		&--image {
+			text-align: center;
 		}
 	}
 
-	.profile-picture {
-		border-radius: 50%;
-		transition: all .7s;
-		
-		&:hover, &:active {
-			box-shadow: .5rem 1rem 2rem rgba(#000, .6);
+	&__map {
+		display: block;
+		padding: 0;
+		margin: 0;
+
+		&--frame {
+			width: 100%;
+			height: 40vh;
 		}
 	}
+}
+
+.profile-picture {
+	border-radius: 50%;
+	transition: all 0.7s;
+
+	&:hover,
+	&:active {
+		box-shadow: 0.5rem 1rem 2rem rgba(#000, 0.6);
+	}
+}
 </style>
--- components/homepage/Landing.vue
+++ components/homepage/Landing.vue
@@ -2,20 +2,47 @@
 	<div class="header">
 		<div class="header__text-container">
 			<div class="header__text-container--main">
-				Hi, I'm <span class="header__text-container--main__name">Dipanjan</span>!
+				Hi, I'm
+				<span class="header__text-container--main__name">Dipanjan</span
+				>!
 			</div>
 			<div class="header__text-container--sub">
-				AI & ML Researcher | Full Stack Developer
+				Full Stack Data Scientist
 			</div>
 			<div class="header__text-container--links">
 				<div class="links">
-					<a target="_blank" href="https://github.com/bacon-delight" class="links__item"><a-icon type="github"/></a>
-					<a target="_blank" href="https://linkedin.com/in/dipanjan131/" class="links__item"><a-icon type="linkedin"/></a>
-					<a target="_blank" href="https://www.instagram.com/bacon_delight/" class="links__item"><a-icon type="instagram"/></a>
-					<a target="_blank" href="https://medium.com/@dipanjan131" class="links__item"><a-icon type="medium"/></a>
+					<a
+						target="_blank"
+						href="https://github.com/bacon-delight"
+						class="links__item"
+						><a-icon type="github"
+					/></a>
+					<a
+						target="_blank"
+						href="https://linkedin.com/in/dipanjan131/"
+						class="links__item"
+						><a-icon type="linkedin"
+					/></a>
+					<a
+						target="_blank"
+						href="https://www.instagram.com/bacon_delight/"
+						class="links__item"
+						><a-icon type="instagram"
+					/></a>
+					<a
+						target="_blank"
+						href="https://medium.com/@dipanjan131"
+						class="links__item"
+						><a-icon type="medium"
+					/></a>
 				</div>
 				<div class="header__text-container--download">
-					<a class="header__text-container--download__link" target="_blank" href="https://drive.google.com/file/d/1ckS-uLeD37rPJMhrtZb8CiJUUrpL8OpW/view?usp=sharing">Download CV</a>
+					<a
+						class="header__text-container--download__link"
+						target="_blank"
+						href="https://drive.google.com/file/d/1ckS-uLeD37rPJMhrtZb8CiJUUrpL8OpW/view?usp=sharing"
+						>Download CV</a
+					>
 				</div>
 			</div>
 		</div>
@@ -23,106 +50,112 @@
 </template>
 
 <script>
-
-	export default {
-		name: 'Landing',
-		mounted () {
-			this.$nextTick(() => {
-				this.$nuxt.$loading.start();
-				setTimeout(() => this.$nuxt.$loading.finish(), 500);
-			})
-		}
-	}
+export default {
+	name: "Landing",
+	mounted() {
+		this.$nextTick(() => {
+			this.$nuxt.$loading.start();
+			setTimeout(() => this.$nuxt.$loading.finish(), 500);
+		});
+	},
+};
 </script>
 
 <style lang="scss" scoped>
-	
-	@import url('https://fonts.googleapis.com/css?family=Raleway&display=swap');
+@import url("https://fonts.googleapis.com/css?family=Raleway&display=swap");
 
-	.header {
-		width: 100vw;
-		height: 100vh;
-		background-image: linear-gradient(
-		    to left bottom,
-		    rgba(#000046, 0.8),
-		    rgba(#1CB5E0, 0.8)),
-			url('../../assets/backgrounds/img02.jpg');
-		background-size: cover;
-		background-position: top;
-		position: relative;
-		clip-path: polygon(0 0, 100% 0, 100% 100%, 0 75%);
+.header {
+	width: 100vw;
+	height: 100vh;
+	background-image: linear-gradient(
+			to left bottom,
+			rgba(#000046, 0.8),
+			rgba(#1cb5e0, 0.8)
+		),
+		url("../../assets/backgrounds/img02.jpg");
+	background-size: cover;
+	background-position: top;
+	position: relative;
+	clip-path: polygon(0 0, 100% 0, 100% 100%, 0 75%);
 
-		&__text-container {
-			position: absolute;
-			top: 45%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			text-align: center;
-			font-family: 'Raleway', sans-serif;
-			color: #ddd;
-			cursor: default;
+	&__text-container {
+		position: absolute;
+		top: 45%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		text-align: center;
+		font-family: "Raleway", sans-serif;
+		color: #ddd;
+		cursor: default;
 
-			&--main {
-				display: block;
-				letter-spacing: .3rem;
-				font-size: 2rem;
+		&--main {
+			display: block;
+			letter-spacing: 0.3rem;
+			font-size: 2rem;
 
-				&__name {
-					font-weight: 800;
-					transition: all 1.2s;
-				}
-
-				&__name:hover {
-					font-weight: 800;
-					color: tomato;
-				}
+			&__name {
+				font-weight: 800;
+				transition: all 1.2s;
 			}
 
-			&--sub {
-				display: block;
-				font-size: 1.2rem;
-				margin: 2rem 0 3rem 0;
-				padding: .7rem 2rem;
-				border: 1px solid #eee;
-				letter-spacing: .2rem;
-				text-transform: uppercase;
+			&__name:hover {
+				font-weight: 800;
+				color: tomato;
 			}
+		}
 
-			&--download {
-				margin-top: 1rem;
-				letter-spacing: .3rem;
-				text-transform: uppercase;
+		&--sub {
+			display: block;
+			font-size: 1.2rem;
+			margin: 2rem 0 3rem 0;
+			padding: 0.7rem 2rem;
+			border: 1px solid #eee;
+			letter-spacing: 0.2rem;
+			text-transform: uppercase;
+		}
 
-				&__link {
-					text-decoration: none;
-					color: #fff;
+		&--download {
+			margin-top: 1rem;
+			letter-spacing: 0.3rem;
+			text-transform: uppercase;
 
-					&:link, &:visited {
-						background-image: linear-gradient(120deg, transparent 0%, transparent 50%, #fff 50%);
-						background-size: 220%;
-						transition: all .4s;
-						padding: .4rem 1rem;
-					}
-					&:hover, &:active {
-						color: #000046;
-						background-position: 100%;
-					}
+			&__link {
+				text-decoration: none;
+				color: #fff;
+
+				&:link,
+				&:visited {
+					background-image: linear-gradient(
+						120deg,
+						transparent 0%,
+						transparent 50%,
+						#fff 50%
+					);
+					background-size: 220%;
+					transition: all 0.4s;
+					padding: 0.4rem 1rem;
+				}
+				&:hover,
+				&:active {
+					color: #000046;
+					background-position: 100%;
 				}
 			}
 		}
 	}
+}
 
-	.links {
-		font-size: 2rem;
+.links {
+	font-size: 2rem;
 
-		&__item {
-			color: tomato;
-			margin: 0 1.4rem;
-			transition: all .9s;
-		}
-
-		&__item:hover {
-			color: #ddd;
-		}
+	&__item {
+		color: tomato;
+		margin: 0 1.4rem;
+		transition: all 0.9s;
 	}
+
+	&__item:hover {
+		color: #ddd;
+	}
+}
 </style>
